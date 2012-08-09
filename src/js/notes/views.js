2aa1d6b99e61b96b76b8c330acb7f7b716ed11fc
Steven Allen <steven@stebalien.com>
2012-08-09 14:52:24 -0400

    Add inline note editor.

--- src/js/notes/views.js
+++ src/js/notes/views.js
@@ -5,16 +5,16 @@
 
     L.make.notes.NoteView = Backbone.View.extend({
         tagName: 'li',
-        className: 'note',
-        initialize: function() {
+        className: 'note hbox justified_box',
+        initialize: function(options) {
             _(this).bindAll();
             var that = this;
+            this.editor = options.editor;
             $(window).one('beforeunload', function() {
                 that.undelegateEvents();
                 that.model.off(null, null, that);
             });
             this.template = L.templates.notes.note;
-            this.className = 'note';
             this.model.on('change:contents', _.mask(this.updateContents, 2), this);
             this.model.on('change:meta', _.mask(this.updateMeta, 2), this);
         },
@@ -37,8 +37,9 @@
         },
         render: function(options) {
             var action = options && options.action;
+
             if (action === 'new') {
-                this.$el.bind('DOMNodeInserted', function() {
+                this.$el.one('DOMNodeInserted', function() {
                     var that = $(this);
                     that.hide();
                     setTimeout(function() {
@@ -46,18 +47,27 @@
                     }, 1);
                 });
             }
+
+            this.$el.bind('DOMNodeRemoved', this.cleanupEditor);
+
             if (this._rendered) {
+                this.cleanupEditor();
                 this.undelegateEvents();
                 this.delegateEvents();
-                return this;
+            } else {
+                this._rendered = true;
+
+                this.updateContents(options);
+                this.updateMeta();
             }
-            this._rendered = true;
 
-            this.updateContents();
-            this.updateMeta();
-
-            // Fade in new notes.
             return this;
+        },
+        cleanupEditor: function() {
+            if (this.editor) {
+                this.$(".editor").html('<textarea></textarea>');
+                delete this.editor;
+            }
         },
         updateMeta: function(options) {
             this.$el.attr('class', this.className);
@@ -69,12 +79,10 @@
             this.$el.html(this.template(this.model.toJSON()));
         },
         events: {
-            'focusin .contents': 'onFocus',
-            'focusout .contents': 'onBlur',
             'click .close-btn': 'removeNote',
             'click .contents': 'onClick',
             'keyup .contents': 'onKeyUp',
-            'keypress .contents': 'onKeyPress'
+            'blur .editor': 'onBlur',
         },
         getNoteText: function() {
             return this.$('.contents').html();
@@ -82,6 +90,7 @@
 
         removeNote: function() {
             this.model.moveTo(L.deletedNotes, {action: 'delete'});
+            return false;
         },
         expand: function() {
             this.$el.css('height', 'auto');
@@ -89,32 +98,81 @@
         collapse: function() {
             this.$el.css('height', '');
         },
+        openEditor: function() {
+            // I know that this is unreadable. But it makes a point.
 
-        onClick: function(e) {
-            if (!e.target.href) {
-                this.$el.find('.contents').focus();
+            var $contentsEl = this.$(".contents"),
+                $editorEl = this.$(".editor");
+            if (!this.editor) {
+                var $textareaEl = $editorEl.children('textarea'),
+                    toolbar = new L.make.omnibox.ToolbarView();
+
+                $editorEl.append(toolbar.render().el);
+
+                var editor = new wysihtml5.Editor($textareaEl.get(0), {
+                        toolbar: toolbar.el,
+                        parserRules: wysihtml5ParserRules,
+                        stylesheets: ['css/reset.css', 'css/wysihtml5.css']
+                    }),
+                    iframe = editor.composer.iframe,
+                    resizeIframe = function() {
+                        var body = $(iframe).contents().find('body'); // Needs document to be loaded.
+                        _.delay(function() {
+                            iframe.style.height = "auto";
+                            iframe.style.height = body.height() + "px";
+                        });
+                    };
+
+                this.editor = editor;
+
+                this.editor.on("keydown", resizeIframe);
+                this.editor.on("focus", resizeIframe);
+                this.editor.on("change", resizeIframe);
+                this.editor.on("blur", this.onBlur);
+
+
+                // Maintain a dialog count so that we don't close the editor with a dialog open.
+                this.editor._dialogCount = 0;
+                this.editor.on("show:dialog", function() {
+                    editor._dialogCount++;
+                });
+                this.editor.on("save:dialog", function() {
+                    editor._dialogCount--;
+                });
+                this.editor.on("cancel:dialog", function() {
+                    editor._dialogCount--;
+                });
             }
-        },
-        onFocus: function() {
-            var contents = this.model.get('contents');
+            this.editor.setValue($contentsEl.html());
+            $editorEl.show();
+            $contentsEl.hide();
+            this.editor.focus();
 
-            this.expand();
-            this.$el.find('.contents').get()[0].contentEditable = true;
-            this.$el.addClass('focus');
-            $('#notes-container').scrollIntoView(this.$el);
-            L.log(LogType.NOTE_EDIT, {
-                noteid: this.model.get('jid'),
-                pinned: (contents.length > 0 && contents[0] === '!')
-            });
         },
         onBlur: function() {
-            this.collapse();
-            this.$el.removeClass('focus');
-            this.$el.find('.contents').get()[0].contentEditable = false;
-            var newContents = this.getNoteText();
+            var that = this;
+            if (!this.editor || this.editor._dialogCount > 0) {
+                return;
+            }
+            _.defer(function() {
+                var focusedEditor = $(document.activeElement).closest(".editor").get(0),
+                    myEditor = that.$('.editor').get(0);
 
-            if (newContents !== this.model.get('contents')) {
-                this.model.changeContents(newContents);
+                if (focusedEditor !== myEditor) {
+                    that.closeEditor();
+                }
+            });
+        },
+        closeEditor: function() {
+            this.model.changeContents(this.editor.getValue());
+            this.collapse();
+            this.$('.editor').hide();
+            this.$('.contents').show();
+        },
+        onClick: function(e) {
+            if (!e.target.href) {
+                this.expand();
+                this.openEditor();
             }
         },
         /**
@@ -124,15 +182,6 @@
             if (event.keyCode === 9) { // TAB
                 event.target.scrollIntoView();
                 $('#notes-container')[0].scrollTop -= 4;
-            }
-        },
-        /**
-        * Save note on shift-enter, without extra line.
-        */
-        onKeyPress: function(event) {
-            if (event.shiftKey && event.keyCode === 13) {
-                event.target.blur();
-                event.preventDefault();
             }
         },
         openLink: function() {
@@ -169,7 +218,7 @@
             });
         },
         events: {
-            'scroll': 'onScroll'
+            'scroll': 'onScroll',
         },
         updateNoteShrinkState : function(model, state) {
             if (!this._rendered) {
@@ -259,12 +308,14 @@
         },
         render: function() {
             if (!this._rendered) {
-                this._rendered = true;
-                this.updateNoteShrinkState(L.options, L.options.get('shrinkNotes'));
                 this.$el.html(this.make('ul', {'class': 'notelist', 'id': 'notes'}));
+                this.updateNoteShrinkState(L.options, L.options.get('shrinkNotes'));
+
+                this._rendered = true;
             }
             this.collection.reset();
             return this;
         }
     });
+
 })(ListIt);
