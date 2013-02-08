494b6bdc010d4f11a2fc84bc57cd658aaceba5e8
Steven Allen <steven@stebalien.com>
2013-02-08 17:56:45 -0500

    Fix images in initial notes.
    fixes #18

--- src/js/setup/upgrade.js
+++ src/js/setup/upgrade.js
@@ -5,11 +5,11 @@ ListIt.lvent.once('setup:upgrade', function(L, lock) {
       var programVersion = L.VERSION,
           defaultNotes = [
             'Open and close list.it by clicking on the <img alt="list-it" ' +
-            'src="img/listit-icon.png"> icon in the upper right hand ' +
+            'src="/img/listit-icon.png"> icon in the upper right hand ' +
             'corner of your screen.',
             'Make new notes or search old ones by typing into the box at the ' +
               'top of list.it\'s sidebar.',
-            'Delete notes by clicking on <img alt="x button" src="img/x.png">.',
+            'Delete notes by clicking on <img alt="x button" src="/img/x.png">.',
             'Edit notes by clicking one and typing, when you select' +
               'something else, the note will automatically save.'
           ],
