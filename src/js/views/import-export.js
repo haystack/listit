0125142c0a4776184fe02b39d4508eafade6ece0
Chongyuan <xiangcy@mit.edu>
2013-04-15 22:51:38 -0400

    Export html format

--- src/js/views/import-export.js
+++ src/js/views/import-export.js
@@ -40,7 +40,7 @@
     },
     // TODO: Should probably define these elsewhere
     types: [
-      {
+      { 
         filename : 'listit-notes.json',
         display : 'JSON',
         exporter: function() {
@@ -71,6 +71,39 @@
             return txt + '* ' + L.util.clean(n.get('contents')).replace(/\n/g, '\n  ') + '\n';
           }, '');
         }
+      },
+      {
+        filename : 'listit-notes.html',
+        display : 'HTML',
+        exporter: function() {
+          var before = "<html>\n"+
+                      "<head>\n"+
+                      "<style>\n"+
+                      "#content\n"+
+                      "{\n"+
+                      "    width: 800px;\n"+
+                      "    margin: 20px auto 0px;\n"+
+                      "    padding: 10px 20px;\n"+
+                      "    border: 1px solid red;\n"+
+                      "}\n"+
+                      "\n"+
+                      "p\n"+
+                      "{\n"+
+                      "    color: gray;\n"+
+                      "}\n"+
+                      "</style>\n"+
+                      "</head>\n"+
+                      "<body>\n"+
+                      "<div id=\"content\">\n"+
+                      "  <ul>\n";
+          var after = "</ul>\n"+
+                      "  </div>\n"+
+                      "</body>\n"+
+                      "</html>";
+          return before + L.notebook.get('notes').reduce(function(txt, n) {
+            return txt + '<li><p>' + L.util.clean(n.get('contents')).replace(/\n/g, '\n  ') + '</p></li>' + '\n';
+          }, '') + after;
+        }
       }
     ],
     importClicked: function() {
@@ -90,9 +123,11 @@
     },
     exportClicked: function() {
       var type = this.types[this.$el.find('#exportSelect').val()];
-      var blob = new BlobBuilder();
-      blob.append(type.exporter());
-      saveAs(blob.getBlob('text/plain;charset=utf-8'), type.filename);
+        var blob = new BlobBuilder();
+        blob.append(type.exporter());
+        saveAs(blob.getBlob('text/plain;charset=utf-8'), type.filename);
     }
   });
 })(ListIt);
+
+
