e08b8b20501b75db2a982808288356ff4c7218ea
Steven Allen <steven@stebalien.com>
2013-06-09 15:42:24 -0400

    Add debugging messages to setup.

--- src/js/setup.js
+++ src/js/setup.js
@@ -4,9 +4,11 @@
   
   var setupActions = [
     function(barr) {
+      debug("setup::begin");
       L.lvent.trigger('setup:before', L, barr);
     },
     function(barr) {
+      debug("setup::upgrade::begin");
       L.lvent.trigger('setup:upgrade:before', L, barr);
     },
     function(barr) {
@@ -14,8 +16,10 @@
     },
     function(barr) {
       L.lvent.trigger('setup:upgrade:after', L, barr);
+      debug("setup::upgrade::end");
     },
     function(barr) {
+      debug("setup::models::begin");
       L.lvent.trigger('setup:models:before', L, barr);
     },
     function(barr) {
@@ -23,10 +27,12 @@
     },
     function(barr) {
       L.lvent.trigger('setup:models:after', L, barr);
+      debug("setup::models::end");
     },
     function(barr) {
       barr.aquire();
       $(function() {
+        debug("setup::views::begin");
         L.lvent.trigger('setup:views:before', L, barr);
         barr.release();
       });
@@ -36,9 +42,11 @@
     },
     function(barr) {
       L.lvent.trigger('setup:views:after', L, barr);
+      debug("setup::views::end");
     },
     function(barr) {
       L.lvent.trigger('setup:after', L, barr);
+      debug("setup::end");
     }
   ];
 
