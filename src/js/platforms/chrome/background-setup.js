d83e2c00c7d6cf1395a0b87b78256d3d4ef0534a
Steven Allen <steven@stebalien.com>
2013-06-12 22:26:48 -0400

    Add option to not use chrome popup
    While this works, it could definently be cleaned up.

--- src/js/platforms/chrome/background-setup.js
+++ src/js/platforms/chrome/background-setup.js
@@ -26,6 +26,42 @@ ListIt.lvent.once('setup:models:after', function(L, barr) {
       //barr.release();
     });
   });
+  var current_popup_id = null;
+  var open_popup = function() {
+    current_popup_id = true;
+    chrome.windows.getCurrent(function(cwin) {
+      chrome.windows.create({
+        url:"index.html",
+        type: "popup",
+        width: 400,
+        height: cwin.height,
+        left: cwin.left-410,
+        top: cwin.top,
+        focused: true
+      }, function(win) {
+        current_popup_id = win.id;
+      });
+    });
+  };
+
+  chrome.browserAction.onClicked.addListener(function() {
+    if (current_popup_id) {
+      chrome.windows.get(current_popup_id, function(win) {
+        if (win && current_popup_id) {
+          chrome.windows.remove(win.id);
+          current_popup_id = null;
+        } else {
+          open_popup()
+        }
+      });
+    } else {
+      open_popup()
+    }
+  });
+  L.preferences.on('change:popup', function(model, value) {
+    chrome.browserAction.setPopup({popup: (value ? "index.html" : '')})
+  });
+  chrome.browserAction.setPopup({popup: (L.preferences.get('popup') ? "index.html" : '')})
 });
 
 ListIt.lvent.once('setup:views:after', function(L, barr) {
---
+++ src/js/platforms/chrome/models/preferences.js
@@ -0,0 +1,9 @@
+(function(L) {
+  L.models.Preferences.prototype.schema.popup = {
+    type: 'boolean',
+    description: 'Use the chrome addon popup instead of a new window.'
+  };
+
+  L.models.Preferences.prototype.defaults.popup = true;
+})(ListIt);
+
