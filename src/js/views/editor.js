6ed9717258ff9e774406c3cfafee3e20c7f2a4a7
Steven Allen <steven@stebalien.com>
2013-06-19 00:30:02 -0400

    More editor stuff.

--- src/js/views/editor.js
+++ src/js/views/editor.js
@@ -4,12 +4,30 @@
   'use strict';
   L.views.Editor = Backbone.View.extend({
     initialize: function(options) {
-      this.initial_content = (options && options.text) || '';
-      this.actions = (options && options.actions);
-      this.toolbarItems = (options && options.toolbarItems) || undefined;
+      if (!options) {
+        options = {}
+      }
+      this.initial_content = options.text || '';
+      this.actions = options.actions;
+      this.toolbarItems = options.toolbarItems;
+      this.autoResize = _.isUndefined(options.autoResize) ? true : options.autoResize;
     },
     className: 'editor',
+    _fixHeight: function() {
+      if (!(this._rendered && this.autoResize)) {
+        return;
+      }
+      var iframe = this.wysihtml5entry.composer.iframe;
+      var body = $(iframe).contents().find('body'); // Needs document to be loaded.
+      var txtbox = this.wysihtml5entry.textareaElement;
+      _.delay(function() {
+        iframe.style.height = 'auto';
+        iframe.style.height = body.height() + 'px';
+        txtbox.style.height = iframe.style.height;
+      });
+    },
     render: function() {
+      var that = this;
       this.$el.html(L.templates["editor"]({ text: this.initial_content}));
       var $bottombar = this.$('.editor-bottombar');
       var $entry = this.$('.editor-entry');
@@ -26,27 +44,15 @@
       this.wysihtml5entry = new wysihtml5.Editor($entry.get(0), {
         toolbar: this.toolbar.el,
         parserRules: wysihtml5ParserRules,
-        style: false,
+        style: true,
         stylesheets: WYSIHTML5_CSS
       });
 
-      var iframe = this.wysihtml5entry.composer.iframe;
-      var txtbox = $entry.get(0);
-
-      var that = this;
-      var resizeEditor = function() {
-        var body = $(iframe).contents().find('body'); // Needs document to be loaded.
-        _.delay(function() {
-          iframe.style.height = 'auto';
-          iframe.style.height = body.height() + 'px';
-          txtbox.style.height = iframe.style.height;
-        });
-      };
 
       // Forward events to parent and resize editor
       _.each(['keydown', 'keyup', 'blur', 'change', 'focus'], function(type) {
         that.wysihtml5entry.on(type, function(evt) {
-          resizeEditor();
+          that._fixHeight();
           if (evt)  {
             // It thinks it's propagating but it really isn't
             _.defer(function() {
@@ -59,9 +65,9 @@
       });
 
       // Focus after inserted.
-      this.$el.on("DOMNodeInsertedIntoDocument", function() {
+      this.wysihtml5entry.on('load', function() {
         _.defer(function() {
-          iframe.contentDocument.body.focus();
+          that.focus();
         });
       });
 
@@ -71,20 +77,25 @@
       this.wysihtml5entry.on('show:dialog', function() {
         that._dialogCount++;
       });
-      this.wysihtml5entry.on('save:dialog', function() {
-        that._dialogCount--;
-      });
-      this.wysihtml5entry.on('cancel:dialog', function() {
+      this.wysihtml5entry.on('hide:dialog', function() {
         that._dialogCount--;
       });
       this._rendered = true;
       return this;
     },
     focus: function() {
-      this.wysihtml5entry.focus();
+      this.wysihtml5entry.focus(true);
+    },
+    blur: function() {
+      // Not provided by wysihtml5.
+      this.wysihtml5entry.currentView.element.blur();
     },
     getText: function() {
-      return this.wysihtml5entry.getValue();
+      if (this._rendered) {
+        return this.wysihtml5entry.getValue();
+      } else {
+        return this.initial_content || "";
+      }
     },
     isShowingDialog: function() {
       return this._dialogCount > 0;
@@ -92,9 +103,15 @@
     setText: function(text) {
       if (this._rendered) {
         this.wysihtml5entry.setValue(text);
+        // Always focus after setting text. Also puts the cursor after the added text.
+        this.wysihtml5entry.focus(true);
+        this._fixHeight();
       } else {
         this.initial_content = text;
       }
+    },
+    appendText: function(text) {
+      this.setText(this.getText() + text);
     },
     clear: function() {
       if (this._rendered) {
