41f125244db314460297ba7cc2914f861b4f74be
Steven Allen <steven@stebalien.com>
2013-06-24 15:41:35 -0400

    Remove duplicate class.

--- src/js/templates.js
+++ src/js/templates.js
@@ -89,7 +89,7 @@ return __p;
 ListIt.templates['note'] = function(obj){
 var __t,__p='',__j=Array.prototype.join,print=function(){__p+=__j.call(arguments,'')};
 with(obj||{}){
-__p+='<div class="icon draggable"></div>\n<div class="editable contents contents flex">'+
+__p+='<div class="icon draggable"></div>\n<div class="editable contents flex">'+
 ((__t=(contents))==null?'':__t)+
 '</div>\n<div class="editor-container flex">\n</div>\n<div class="close-btn clickable"></div>\n';
 }
