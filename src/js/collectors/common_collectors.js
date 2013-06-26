434cbd804d32e74df74bfe11e35df4708a42eca0
Steven Allen <steven@stebalien.com>
2013-06-26 18:12:45 -0400

    Better tag matching.
    closes #58

--- src/js/collectors/common_collectors.js
+++ src/js/collectors/common_collectors.js
@@ -23,7 +23,18 @@
             n.cut('span.listit_tag');
             var tags = [];
             note.contents = n.html().replace(
-              /(^|[\s\(\[,])#([a-zA-Z][a-zA-Z0-9]{2,})\b/g,
+              /* This regex either of the following:
+               * 1. The beginning of the line and then a tag.
+               * 2. a space, nonbreaking space, a tag, or any of `([<,{"'`
+               *    any number of tags without "content";
+               *    and then a tag.
+               *    and then nothing or not a tag.
+               *
+               * Where a tag is #[letters and numbers].
+               *   The first character must be a letter
+               *   there must be three letters
+               */
+              /(^|(?:(?:&nbsp;|<[^>]*>|[\s\(\[<,{"'])))#([a-zA-Z][a-zA-Z0-9]{2,})\b/g,
               function(match, prefix, tag) {
 
                 tags.push(tag);
