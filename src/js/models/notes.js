db1d965827a4c634f7ea2e13d93c5470d6d317b9
Haley B-E <hbrandterichsen@gmail.com>
2013-06-26 14:32:59 -0400

    removed extra commas from CSV export

--- src/js/models/notes.js
+++ src/js/models/notes.js
@@ -362,17 +362,17 @@
         csv: {
           display : 'CSV',
           exporter: function(notebook) {
-            var CSVHeader = '"Contents","Deleted","Modified","ID","Version","Created","Edited","Meta",\n';
+            var CSVHeader = '"Contents","Deleted","Modified","ID","Version","Created","Edited","Meta"\n';
             var activeNotes = notebook.get('notes').reduce(function(txt, n) {                
                 return txt + '"' + n.get('contents').replace(/"/g, '""') + '"' + ',FALSE,' 
                 + n.get('modified') + ',' + n.get('id') + ',' + n.get('version') + ',' + new Date(n.get('created')) + ','
-                + new Date(n.get('edited')) + ','+ '"' + JSON.stringify(n.get('meta')).replace(/"/g, '""') + '"' + ','
+                + new Date(n.get('edited')) + ','+ '"' + JSON.stringify(n.get('meta')).replace(/"/g, '""') + '"'
                 + '\n';
             }, '');
             var deletedNotes = notebook.get('deletedNotes').reduce(function(txt, n) {
                 return txt + '"' + n.get('contents').replace(/"/g, '""') + '"' + ',TRUE,' 
                 + n.get('modified') + ',' + n.get('id') + ',' + n.get('version') + ',' + new Date(n.get('created')) + ','
-                + new Date(n.get('edited')) + ','+ '"' + JSON.stringify(n.get('meta')).replace(/"/g, '""') + '"' + ','
+                + new Date(n.get('edited')) + ','+ '"' + JSON.stringify(n.get('meta')).replace(/"/g, '""') + '"'
                 + '\n';
             }, '');
             return CSVHeader + activeNotes + deletedNotes;
