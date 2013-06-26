fe0fee95525d30b14af720aeb1f4f2cb64ef0ea8
Haley B-E <hbrandterichsen@gmail.com>
2013-06-26 13:57:38 -0400

    added deleted notes to csv export

--- src/js/models/notes.js
+++ src/js/models/notes.js
@@ -362,13 +362,20 @@
         csv: {
           display : 'CSV',
           exporter: function(notebook) {
-            var CSVHeader = '"Contents","Modified","ID","Version","Created","Edited","Meta",\n';
-            return  CSVHeader + notebook.get('notes').reduce(function(txt, n) {                
-                return txt + '"' + n.get('contents').replace(/"/g, '""') + '"' + ',' 
+            var CSVHeader = '"Contents","Deleted","Modified","ID","Version","Created","Edited","Meta",\n';
+            var activeNotes = notebook.get('notes').reduce(function(txt, n) {                
+                return txt + '"' + n.get('contents').replace(/"/g, '""') + '"' + ',FALSE,' 
                 + n.get('modified') + ',' + n.get('id') + ',' + n.get('version') + ',' + new Date(n.get('created')) + ','
                 + new Date(n.get('edited')) + ','+ '"' + JSON.stringify(n.get('meta')).replace(/"/g, '""') + '"' + ','
                 + '\n';
             }, '');
+            var deletedNotes = notebook.get('deletedNotes').reduce(function(txt, n) {
+                return txt + '"' + n.get('contents').replace(/"/g, '""') + '"' + ',TRUE,' 
+                + n.get('modified') + ',' + n.get('id') + ',' + n.get('version') + ',' + new Date(n.get('created')) + ','
+                + new Date(n.get('edited')) + ','+ '"' + JSON.stringify(n.get('meta')).replace(/"/g, '""') + '"' + ','
+                + '\n';
+            }, '');
+            return CSVHeader + activeNotes + deletedNotes;
           }
         }
       },
