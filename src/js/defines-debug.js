8be05b408a006f5027803e32847176c963add8b7
Steven Allen <steven@stebalien.com>
2013-07-09 18:17:49 -0400

    Set defines debug to true for debug mode.

--- src/js/defines-debug.js
+++ src/js/defines-debug.js
@@ -1,5 +1,5 @@
 
-                DEBUG_MODE=false;
+                DEBUG_MODE=true;
                 WYSIHTML5_CSS=["css/reset.css", "css/wysihtml5-iframe.css", "css/note-content.css"];
                 LISTIT_CREDITS=["Steven Allen","Wolfe Styke","electronic max","Prof. David Karger"];
                 LISTIT_EMAIL="listit@csail.mit.edu";
