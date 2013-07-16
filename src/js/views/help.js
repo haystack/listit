a698dc346b02a827b9cd9c917de4f616c25c9d07
deannah <deannah@mit.edu>
2013-07-16 10:42:16 -0400

    Help page with contents created for chrome and webapp.

---
+++ src/js/views/help.js
@@ -0,0 +1,24 @@
+(function(L) {
+  'use strict';
+
+  L.views.HelpPage = Backbone.View.extend({
+    id: 'page-help',
+    className: 'page',
+    initialize: function(options) {
+      if (options) {
+        this.platform = options.platform;
+      }
+    },
+    render: function() {
+      this.$el.html(L.templates["pages/help"]());
+      
+      if(this.platform) {
+        var main = this.$('#help-main');
+        var platformHelp = L.templates["platforms/" + this.platform + "/help"]();
+        main.append(platformHelp);
+      }
+      
+      return this;
+    }
+  });
+})(ListIt);
