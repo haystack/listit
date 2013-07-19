b8a441ead31d4be91d31c65976266d9ea9ab988e
deannah <deannah@mit.edu>
2013-07-19 14:43:42 -0400

    Adding help button to controls, adjusting help content.

--- src/js/platforms/chrome/sidebar-setup.js
+++ src/js/platforms/chrome/sidebar-setup.js
@@ -12,6 +12,11 @@ ListIt.lvent.once('setup:views:after', function(L, barr) {
     this.navigate('', {trigger: false});
     window.open('/options.html', '_new');
   });
+  
+  L.router.route('help', 'help', function() {
+    this.navigate('', {trigger: false});
+    window.open('/help.html', '_new');
+  });
 
   // Close on hotkey press.
   chrome.commands.getAll(function(cmds) {
