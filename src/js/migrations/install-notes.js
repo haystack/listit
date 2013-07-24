fe2429af4f0a45631c4a80e2cc2f7210a0c62cc7
deannah <deannah@mit.edu>
2013-07-24 17:38:33 -0400

    Fixing up default notes.

--- src/js/migrations/install-notes.js
+++ src/js/migrations/install-notes.js
@@ -2,16 +2,18 @@
 ListIt.gvent.on('initialize', function(L, version, barr) {
   'use strict';
   var defaultNotes = [
-    'Make new notes or search old ones by typing into the box above.',
-    'Make notes easier to find by including <span class="listit_tag">#tags.' +
-      '</span>',
-    'Save a new note by clicking <img title="Save" src="/img/actions/add.png">',
+    'Make new notes or search old ones by typing into the box above. Save ' +
+      'a new note by clicking <img title="Save" src="/img/actions/add.png">',
     'Pin notes to keep them at the top of your list. Pin a new note by ' +
       'clicking <img title="Pin" src="/img/actions/add_pinned.png"> to save ' +
       'with a pin. Change whether an old note is pinned by clicking the star.',
     'Delete notes by clicking on <img title="x button" src="/img/close.png">.',
-    'Edit notes by clicking one and typing.',
-    'Move notes by clicking and dragging them to new locations.',
+    'Edit notes by clicking one and typing. They will save automatically.',
+    'Reorder notes by clicking and dragging them to new locations.',
+    'Make a List.it account by going to the options page. This will allow ' +
+      'you to sync your notes onto our server. You can also opt in to our' +
+      'studies on how people take notes. This will allow us to make a ' +
+      'better program for you.',
     'Learn more about using List.it by clicking the question mark in the upper'+
       ' right corner of this sidebar to visit the help page.'
   ];
