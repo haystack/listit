c3067f4bbc8252085332126cfbbde9cd79418372
deannah <deannah@mit.edu>
2013-07-30 18:04:26 -0400

    Trying to make hotkey close sidebar on windows chrome.

--- src/js/platforms/chrome/sidebar-setup.js
+++ src/js/platforms/chrome/sidebar-setup.js
@@ -22,7 +22,25 @@ ListIt.lvent.once('setup:views:after', function(L, barr) {
   chrome.commands.getAll(function(cmds) {
     var cmd = _.findWhere(cmds, {name: "_execute_browser_action"});
     if (cmd && cmd.shortcut) {
-      $(document).bind('keydown', cmd.shortcut, _.bind(window.close, window));
+      //$(document).bind('keydown', cmd.shortcut, _.bind(window.close, window));
+      // $(window).bind('keydown', cmd.shortcut, function() {
+        // console.log("you're pressing the command dawg");
+        // document.close();
+      // });
+      // $(window).on('keydown', null, cmd.shortcut.toLocaleLowerCase(), function() {
+        // console.log("you pressed the key, yay");
+        // document.close();
+        // });
+      $(document).on('keydown', null, cmd.shortcut, function() {
+        window.close();
+      });
+      $(document).on('keydown', null, "Ctrl+t", function() {
+        window.close();
+      });
+      $(window).on('keydown', null, "ctrl+shift+l", function() {
+        window.close();
+      });
+      $(document).bind('keydown', "ctrl+m", _.bind(window.close, window));
     }
   });
 });
