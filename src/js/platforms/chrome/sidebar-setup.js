7658bb44ef0b24c0041f3a56ef1d211f837027bb
deannah <deannah@mit.edu>
2013-07-30 18:10:43 -0400

    Removing keybinding on chrome hotkey

--- src/js/platforms/chrome/sidebar-setup.js
+++ src/js/platforms/chrome/sidebar-setup.js
@@ -17,30 +17,4 @@ ListIt.lvent.once('setup:views:after', function(L, barr) {
     this.navigate('', {trigger: false});
     window.open('/help.html', '_new');
   });
-
-  // Close on hotkey press.
-  chrome.commands.getAll(function(cmds) {
-    var cmd = _.findWhere(cmds, {name: "_execute_browser_action"});
-    if (cmd && cmd.shortcut) {
-      //$(document).bind('keydown', cmd.shortcut, _.bind(window.close, window));
-      // $(window).bind('keydown', cmd.shortcut, function() {
-        // console.log("you're pressing the command dawg");
-        // document.close();
-      // });
-      // $(window).on('keydown', null, cmd.shortcut.toLocaleLowerCase(), function() {
-        // console.log("you pressed the key, yay");
-        // document.close();
-        // });
-      $(document).on('keydown', null, cmd.shortcut, function() {
-        window.close();
-      });
-      $(document).on('keydown', null, "Ctrl+t", function() {
-        window.close();
-      });
-      $(window).on('keydown', null, "ctrl+shift+l", function() {
-        window.close();
-      });
-      $(document).bind('keydown', "ctrl+m", _.bind(window.close, window));
-    }
-  });
 });
