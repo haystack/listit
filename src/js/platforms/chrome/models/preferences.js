3f052e6b9a86abc991d0d911099f50c9df0a43c0
Steven Allen <steven@stebalien.com>
2013-07-30 13:50:03 -0400

    [chrome] Make "tabs" permission optional

--- src/js/platforms/chrome/models/preferences.js
+++ src/js/platforms/chrome/models/preferences.js
@@ -8,12 +8,41 @@
     sidebarTrackWindow: {
       type: 'boolean',
       description: "Make list.it's window track the main window (may drain battery)"
+    },
+    recordActiveTab: {
+      type: 'boolean',
+      description: "Record the active tab's URL in note metadata on creation."
     }
   }, L.models.Preferences.prototype.schema);
 
   L.models.Preferences.prototype.defaults = _.defaults({
     popup: false,
-    sidebarTrackWindow: false
+    sidebarTrackWindow: false,
+    recordActiveTab: false
   }, L.models.Preferences.prototype.defaults);
 })(ListIt);
+
+ListIt.lvent.once('setup:models:after', function(L, barr) {
+  L.preferences.ready(function() {
+    L.preferences.on('change:recordActiveTab', function(m, v) {
+      try {
+        if (v) {
+          // This only works from event callbacks. This shouldn't be a problem
+          // but is kind of hacky.
+          chrome.permissions.request({
+            permissions: ['tabs'],
+          }, function(granted) {
+            if (!granted) {
+              L.preferences.set('recordActiveTab', false);
+            }
+          });
+        } else {
+          chrome.permissions.remove({
+            permissions: ['tabs'],
+          });
+        }
+      } catch (e) {}
+    });
+  });
+});
 
