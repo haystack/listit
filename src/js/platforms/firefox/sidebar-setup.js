bd27146749f4c73e618a7d6f7c5672f2be2e9799
Steven Allen <steven@stebalien.com>
2013-08-07 14:35:25 -0400

    Don't bother supporting browsers without flexbox
    Other things break anyways.

--- src/js/platforms/firefox/sidebar-setup.js
+++ src/js/platforms/firefox/sidebar-setup.js
@@ -16,18 +16,4 @@ ListIt.lvent.once('setup:views:after', function(L, barr) {
     var browser = window.top.getBrowser();
     browser.selectedTab = browser.addTab('chrome://listit/content/webapp/help.html');
   });
-  var fixSize = function() {
-    var h = $(this);
-    var p = h.parent();
-    var c = p.children('.contents');
-    c.height(p.innerHeight()-h.outerHeight());
-  };
-  $(".page>.header").on("resize", fixSize);
-
-  _.defer(function() {
-    $(".page>.header").each(function() {
-      fixSize.apply(this);
-      $(window).on('resize', _.bind(fixSize, this));
-    });
-  });
 });
--- src/js/platforms/web/web-setup.js
+++ src/js/platforms/web/web-setup.js
@@ -3,24 +3,4 @@ ListIt.lvent.once('setup:views', function(L, barr) {
   L.addPage('main', new L.views.MainPage());
   L.addPage('options', new L.views.OptionsPage());
   L.addPage('help', new L.views.HelpPage());
-
-  // Ugly hack for firefox -moz-box. Need this event to fixup page header
-  // (omnibox) /content (notelist) heights.
-  // BUG: https://bugzil.la/579776 (the workarround doesn't work).
-  if (!Modernizr.flexbox) {
-    var fixSize = function() {
-      var h = $(this);
-      var p = h.parent();
-      var c = p.children('.contents');
-      c.height(p.innerHeight()-h.outerHeight());
-    };
-    $(".page>.header").on("resize", fixSize);
-
-    _.defer(function() {
-      $(".page>.header").each(function() {
-        fixSize.apply(this);
-        $(window).on('resize', _.bind(fixSize, this));
-      });
-    });
-  }
 });
