a0e04aec98057ec69483a99c8cc3fc6f6c9304d1
deannah <deannah@mit.edu>
2013-08-07 17:12:47 -0400

    [Firefox] Making jshint happy.

--- src/js/platforms/firefox/icon-manager.js
+++ src/js/platforms/firefox/icon-manager.js
@@ -1,3 +1,6 @@
+/*jshint globalstrict: true*/
+/*exported EXPORTED_SYMBOLS, ListItIM*/
+
 // Handles the toolbar button.
 
 'use strict';
@@ -6,19 +9,17 @@ var EXPORTED_SYMBOLS = ["ListItIM"];
 
 var ListItIM = {};
 
-// Creates the dom element for the toolbar button, and the basis for adding it to a toolbar.
-// var createIcon = function(window, enabling) {
-  // var document = window.document;
-  // var button = document.createElement("toolbarbutton");
-  // button.setAttribute("id", "listitButton");
-  // button.setAttribute("command", "viewListitSidebar");
-  // button.setAttribute("observes", "viewListitSidebar");
-  // button.setAttribute("key", "key_viewListitSidebar");
-  // button.setAttribute("image", "chrome://listit/content/webapp/img/icon16.png");
-  // button.setAttribute("class", "listit toolbarbutton-1 chromeclass-toolbar-additional");
-  // document.getElementById("navigator-toolbox").palette.appendChild(button);
-  // addIcon(document, button, enabling);
-// };
+// if extension is being first enabled, add the icon to the default position.
+var useDefaultPosition = function(document, button) {
+  var defaultToolbar = "addon-bar";
+  var toolbar = document.getElementById(defaultToolbar);
+  toolbar.insertItem(button.id);
+  var currentset = toolbar.getAttribute("currentset") + "," + button.id;
+  toolbar.setAttribute("currentset", currentset);
+  toolbar.currentSet = currentset;
+  document.persist(defaultToolbar, "currentset");
+  toolbar.collapsed = false;
+};
 
 var addIcon = function(document, button, enabling) {
   // Check which (if any) toolbar the button should be located in.
@@ -33,7 +34,7 @@ var addIcon = function(document, button, enabling) {
       break;
     }
   }
-  
+
   // Puts the button into whichever toolbar it belongs in:
   if (toolbar) {
     var itemAfter = document.getElementById(currentset[idx+1]);
@@ -43,18 +44,6 @@ var addIcon = function(document, button, enabling) {
     // add the button to the default position:
     useDefaultPosition(document, button);
   }
-};
-
-// if extension is being first enabled, add the icon to the default position.
-var useDefaultPosition = function(document, button) {
-  var defaultToolbar = "addon-bar";
-  var toolbar = document.getElementById(defaultToolbar);
-  toolbar.insertItem(button.id);
-  var currentset = toolbar.getAttribute("currentset") + "," + button.id;
-  toolbar.setAttribute("currentset", currentset);
-  toolbar.currentSet = currentset;
-  document.persist(defaultToolbar, "currentset");
-  toolbar.collapsed = false;
 };
 
 ListItIM.createButton = function(window, enabling) {
