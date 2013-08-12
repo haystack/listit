ed87302358eafd7bb0a209cd262803d52cca7289
Haley B-E <hbrandterichsen@gmail.com>
2013-08-12 15:07:50 -0400

    options and help pages are consistent (old style)
    update to new style (all pages)

--- src/js/templates.js
+++ src/js/templates.js
@@ -252,7 +252,7 @@ return __p;
 ListIt.templates['pages/options'] = function(obj){
 var __t,__p='',__j=Array.prototype.join,print=function(){__p+=__j.call(arguments,'')};
 with(obj||{}){
-__p+='<div id="options-header">\n    <h2 class="options-title">List.it</h2>\n    <a id="showListLink" href="#">Take me back to my notes</a>\n</div>\n<div id="options-body"></div>\n\n';
+__p+='<div id="options-header">\n    <h2 class="options-title">List.it</h2>\n    <a id="showListLink" href="#">Take me back to my notes</a>\n</div>\n<div id="title-circle"></div>\n<div id="options-body"></div>\n\n';
 }
 return __p;
 };
