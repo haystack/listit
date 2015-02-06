3b2027873361f90483516f0479013a5f2bdf3caa
Steven Allen <steven@stebalien.com>
2015-02-06 17:22:45 -0500

    Update defines-debug.js

--- src/js/defines-debug.js
+++ src/js/defines-debug.js
@@ -1,6 +1,6 @@
 
                 DEBUG_MODE=true;
                 WYSIHTML5_CSS=["css/reset.css", "css/wysihtml5-iframe.css", "css/note-content.css"];
-                LISTIT_CREDITS=["Prof. David Karger","electronic max","Wolfe Styke","Steven Allen","Chongyuan Xiang","Deanna M Heer","Haley Brandt-Erichsen"];
+                LISTIT_CREDITS=["Prof. David Karger","electronic max","Wolfe Styke","Steven Allen","Chongyuan Xiang","Deanna M Heer","Haley Brandt-Erichsen","picol.org (Some icons. See source code.)"];
                 LISTIT_EMAIL="listit@csail.mit.edu";
             
